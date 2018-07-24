a6d15efc5521f674b7a286172abf8bd040e84522
joens16 <joens16@hotmail.com>
2018-07-24 14:55:37 -0500

    More features implemented

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require('cli-table');
+// var Table = require('cli-table');
 
 
 var connection = mysql.createConnection({
@@ -20,20 +20,29 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  selectAll();
+  displayItems();
 });
 
 
-function selectAll() {
+var displayItems = () => {
   connection.query(`SELECT * FROM products`, (err, res) => {
     if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      console.log(" - - - - - - - - - - - - -  ")
+      console.log("item number: " + res[i].item_id)
+      console.log("item: " + res[i].product_name)
+      console.log("price: $" + res[i].price)
 
-    console.log(res);
-    connection.end();
+      // console.log(res);
+      // connection.end();
+    }
     itemsToBuy();
   })
 }
 
+var validator = (value) => {
+
+}
 // // instantiate
 // var table = new Table({
 //   head: ['TH 1 label', 'TH 2 label']
@@ -46,20 +55,43 @@ function selectAll() {
 // , ['First value', 'Second value']
 // );
 
-var itemsToBuy = function () {
+var itemsToBuy = () => {
   inquirer
     .prompt([
       // Here we create a basic text prompt.
       {
         type: "input",
         message: "What is your the ID of the product you would like to buy?",
-        name: "ID"
+        name: "item_id",
+        filter: Number
       },
       {
         type: "input",
         message: "How many units of this product would you like to buy??",
-        name: "Units"
+        name: "Units",
+        filter: Number
       }
     ])
-}
+    .then(function (itemsToBuy) {
+      var items = itemsToBuy.item_id
+      var quantity = itemsToBuy.item_id
 
+      var theQuery = 'SELECT * FROM products WHERE ?';
+
+      connection.query(theQuery, { item_id: items }, function (err, res) {
+        if (err) throw err
+
+        if (res.lenght === 0){
+          console.log("Not a valid ID. Please try again")
+          displayItems()
+        }
+        else {
+          var productDescription = res[0]
+
+          if (quantity <= productDescription.stock_quantity){
+            console.log(productDescription.product_name + " - This product is available. Please place an order!")
+          }
+        }
+      })
+    })
+}
