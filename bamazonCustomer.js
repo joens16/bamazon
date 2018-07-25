b8223a8d8cae9371e137ee230b28a3166f6fc321
joens16 <joens16@hotmail.com>
2018-07-25 18:27:19 -0500

    Done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-// var Table = require('cli-table');
+var Table = require('cli-table');
 
 
 var connection = mysql.createConnection({
@@ -20,27 +20,27 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  displayItems();
+  showAsTable();
 });
 
 
-var displayItems = () => {
-  connection.query(`SELECT * FROM products`, (err, res) => {
-    if (err) throw err;
-    for (let i = 0; i < res.length; i++) {
-      console.log(" - - - - - - - - - - - - -  ")
-      console.log("item number: " + res[i].item_id)
-      console.log("item: " + res[i].product_name)
-      console.log("price: $" + res[i].price)
-      console.log("Quantity Available: " + res[i].stock_quantity)
-      console.log(" - - - - - - - - - - - - -  ")
+// var displayItems = () => {
+//   connection.query(`SELECT * FROM products`, (err, res) => {
+//     if (err) throw err;
+//     for (let i = 0; i < res.length; i++) {
+//       console.log(" - - - - - - - - - - - - -  ")
+//       console.log("item number: " + res[i].item_id)
+//       console.log("item: " + res[i].product_name)
+//       console.log("price: $" + res[i].price)
+//       console.log("Quantity Available: " + res[i].stock_quantity)
+//       console.log(" - - - - - - - - - - - - -  ")
 
-      // console.log(res);
-      // connection.end();
-    }
-    itemsToBuy();
-  })
-}
+//       // console.log(res);
+//       // connection.end();
+//     }
+//     itemsToBuy();
+//   })
+// }
 
 // // instantiate
 // var table = new Table({
@@ -78,42 +78,8 @@ var itemsToBuy = () => {
       var quantity = answer.units;
 
       verifyStock(items, quantity);
-     
-    //   var theQuery = 'SELECT * FROM products WHERE ?';
 
-    //   connection.query(theQuery, items, function (err, res) {
-    //     if (err) throw err
 
-    //     if (res.lenght === 0) {
-    //       console.log("Not a valid ID. Please try again");
-    //       displayItems();
-    //     }
-    //     else {
-    //       // var productDescription = res[0]
-
-    //       // if (quantity <= productDescription.stock_quantity) {
-    //       //   console.log(" - - - - - - - - - - - - -  ")
-    //       //   console.log(productDescription.product_name + " This product is available. Please place an order!")
-    //       //   console.log(" - - - - - - - - - - - - -  ")
-
-    //       //   var queryUpdate = "UPDATE products SET stock_quantity = " + (productDescription.stock_quantity - quantity) + "WHERE item_id = " + items
-
-    //       //   connection.query(queryUpdate, function (err, data) {
-    //       //     if (err) throw err;
-    //       //     console.log(" - - - - - - - - - - - - -  ")
-    //       //     console.log("Congrats! Thank you for placing an order! Your total will be $" + quantity * productDescription.price)
-    //       //     console.log(" - - - - - - - - - - - - -  ")
-    //       //     connection.end();
-
-    //       //   })
-    //       // } else {
-    //       //   console.log(" - - - - - - - - - - - - -  ")
-    //       //   console.log("We apologize. At this time we do not have that quantity available. Please adjust your order for " + productDescription.product_name + "s")
-    //       //   console.log(" - - - - - - - - - - - - -  ")
-
-    //       // }
-    //     }
-    //   });
     });
 }
 
@@ -125,11 +91,13 @@ function verifyStock(id, count) {
       var quantity = res[i].stock_quantity;
       if (quantity < count) {
         console.log("We apologize. At this time we do not have that quantity available. Please adjust your order for " + res[i].product_name + "s");
+        console.log(" - - - - - - - - - - - - -  ");
+        itemsToBuy();
       } else {
         updateStock(id, count);
         totalPrice(id, count);
       }
-    }  
+    }
 
   });
 }
@@ -142,19 +110,19 @@ function updateStock(id, count) {
       var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
       connection.query(query, [newQuantity, id], function (err, res2) {
         if (err) throw err;
-  
+
       });
-  
+
     }
   });
 
 }
 
 
-function totalPrice(id, count){
+function totalPrice(id, count) {
   var query = "SELECT price FROM products WHERE item_id = ?";
   connection.query(query, [id], function (err, res) {
-    for (let i = 0; i < res.length; i++){
+    for (let i = 0; i < res.length; i++) {
       var yourPrice = res[i].price * parseFloat(count);
       console.log(`Your total price is: ${yourPrice} dollars`);
     }
@@ -162,4 +130,25 @@ function totalPrice(id, count){
   });
 
 }
+
+function showAsTable() {
+  var query = "SELECT * FROM products";
+  connection.query(query, function (err, res) {
+
+    var table = new Table({
+      chars: {
+        'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+        , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+        , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+        , 'right': '║', 'right-mid': '╢', 'middle': '│'
+      }, head: ['ID', 'PRODUCT', 'DEPARTMENT', 'PRICE', 'IN STOCK']
+    });
+    for (let i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+    }
+    console.log(table.toString());
+    itemsToBuy();
+  });
+}
+
 
